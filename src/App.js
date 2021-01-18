6e696d3226c9fc8821b01e514db9a338a82e5e6e
claudiahalip <claudia4ro@yahoo.com>
2021-01-17 20:59:45 -0600

    comonents created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
+import NavBar from './components/NavBar'
+
 
 function App() {
   return (
     <div>
-     <h1>Claudia Vamesu Halip</h1>   
+     <h1>Claudia Vamesu Halip</h1>  
+     <Home/>
+     <Resume/>
+     <Projects/> 
+     <NavBar/>
     </div>
   );
 }
