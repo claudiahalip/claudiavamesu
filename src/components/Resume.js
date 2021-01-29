c486a335a3160c4336d14116b96f81a6bfaa7ebf
claudiahalip <claudia4ro@yahoo.com>
2021-01-29 15:51:21 -0600

    resume & projects

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import resume from '../images/resume.jpg'
+import resume from '../images/resume.png'
 class Resume extends Component {
     render (){
         return(
             <div>
                <h2>Resume:</h2>
-               <a href = "https://docs.google.com/document/d/1RVAdvHZDqCUlD1AsmmaC7uitJXe-ZJDLJFBB68IfIis/edit">
+               <a href = "https://docs.google.com/document/d/1V-ta_5yjsvUN1UQfg-8nNxna-4AdTUS5/edit">
                <img className = "resume" src={resume}></img>
                </a>
                
