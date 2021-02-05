2022b3568e67c78a8688c56869215340337f9dc2
claudiahalip <claudia4ro@yahoo.com>
2021-02-04 20:24:35 -0600

    my story edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,12 +11,12 @@ class Home extends Component {
 
               <p className="story">
               I live in the Chicago suburbs. I recently graduated from Flatiron school, currently working on my own projects where I can practice my skills gained during my time there.
-              I’ve graduate from college many years ago with a bachelor’s degree in Public Administration
-              but I’ve always known I wanted to work in tech. This year I decided to change my career.
-              Before this change I worked as a photographer. I've started as a photojournalist for a local newspaper. Through photography I traveled the world, worked on cruise ships, and met many interesting people.
-              Years later, my husband and I created H Photography, our own business which operare in Europe and USA.
-              When I'm not coding or photographing I like to play piano, I teach myself to play piano, I'm reading and also spent time with my family.
-              I am looking for opportunities where I can improve my knowledge and grow in this big and fascinated field of technology.
+              I’ve graduated from college many years ago with a bachelor’s degree in Public Administration
+              but I’ve always known that I wanted to work in tech. This year I decided to change my career...
+              Before this change I worked as a photographer. I started as a photojournalist for a local newspaper. Through photography I traveled the world, worked on cruise ships, and met many interesting people.
+              Years later, my husband and I created H Photography, our own business which operates in Europe and USA.
+              When I'm not coding or photographing I like to play guitar, I'm teaching myself to play piano, I read, and also spend time with my family.
+              I am looking for opportunities where I can improve my knowledge and grow in this big and fascinating field of technology.
               </p>       
             </div>
        )
